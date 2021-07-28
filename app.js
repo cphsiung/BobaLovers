fdcf205f60b47d0a9481d0e81dad6ca783d591f8
cphsiung <chunpeihs@gmail.com>
2021-07-27 21:18:04 -0700

    Add error validation

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate')
+const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const {bobaSchema} = require('./schemas.js')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Boba = require('./models/boba');
 
@@ -26,46 +30,86 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateBoba = (req, res, next) => {
+  const {error} = bobaSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(', ')
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/bobas', async (req, res) => {
-  const bobas = await Boba.find({});
-  res.render('bobas/index', { bobas });
-});
+app.get(
+  '/bobas',
+  catchAsync(async (req, res) => {
+    const bobas = await Boba.find({});
+    res.render('bobas/index', { bobas });
+  })
+);
 
 app.get('/bobas/new', (req, res) => {
   res.render('bobas/new');
 });
 
-app.post('/bobas', async (req, res) => {
-  const boba = new Boba(req.body.boba);
-  await boba.save();
-  res.redirect(`/bobas/${boba._id}`);
+app.post(
+  '/bobas', validateBoba,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.boba) throw new ExpressError('Invalid Boba Place Data', 400);
+    
+    const boba = new Boba(req.body.boba);
+    await boba.save();
+    res.redirect(`/bobas/${boba._id}`);
+  })
+);
+
+app.get(
+  '/bobas/:id',
+  catchAsync(async (req, res) => {
+    const boba = await Boba.findById(req.params.id);
+    res.render('bobas/show', { boba });
+  })
+);
+
+app.get(
+  '/bobas/:id/edit',
+  catchAsync(async (req, res) => {
+    const boba = await Boba.findById(req.params.id);
+    res.render('bobas/edit', { boba });
+  })
+);
+
+app.put(
+  '/bobas/:id/', validateBoba,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const boba = await Boba.findByIdAndUpdate(id, { ...req.body.boba });
+    res.redirect(`/bobas/${boba._id}`);
+  })
+);
+
+app.delete(
+  '/bobas/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Boba.findByIdAndDelete(id);
+    res.redirect('/bobas');
+  })
+);
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
 });
 
-app.get('/bobas/:id', async (req, res) => {
-  const boba = await Boba.findById(req.params.id);
-  res.render('bobas/show', { boba });
+app.use((err, req, res, next) => {
+  const {statusCode = 500} = err;
+  if (!err.message) err.message = 'Something went wrong!';
+  res.status(statusCode).render('error', { err });
 });
-
-app.get('/bobas/:id/edit', async (req, res) => {
-  const boba = await Boba.findById(req.params.id);
-  res.render('bobas/edit', { boba });
-});
-
-app.put('/bobas/:id/', async (req, res) => {
-  const { id } = req.params;
-  const boba = await Boba.findByIdAndUpdate(id, { ...req.body.boba });
-  res.redirect(`/bobas/${boba._id}`);
-});
-
-app.delete('/bobas/:id', async (req, res) => {
-  const { id } = req.params;
-  await Boba.findByIdAndDelete(id);
-  res.redirect('/bobas');
-})
 
 app.listen(3000, () => {
   console.log('Serving on port 3000');
