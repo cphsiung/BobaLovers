333e5dfaf7d700aa393b0b9acd3d9968c1a0a5b4
cphsiung <chunpeihs@gmail.com>
2021-08-01 23:08:51 -0700

    Add authentication

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const bobas = require('./routes/bobas');
 const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/boba-lovers', {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-	console.log('Database connected');
+  console.log('Database connected');
 });
 
 const app = express();
@@ -34,42 +37,49 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
-	secret: 'thisshouldbeabettersecret',
-	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		httpOnly: true,
-		expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //expire in a week in milliseconds
-		maxAge: 1000 * 60 * 60 * 24 * 7
-	}
-}
-app.use(session(sessionConfig))
-app.use(flash())
+  secret: 'thisshouldbeabettersecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //expire in a week in milliseconds
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //set up middleware for every success request to show success flash message
 app.use((req, res, next) => {
-	res.locals.success = req.flash('success');
-	res.locals.error = req.flash('error');
-	next();
-})
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use('/bobas', bobas);
 app.use('/bobas/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
-	res.render('home');
+  res.render('home');
 });
 
 app.all('*', (req, res, next) => {
-	next(new ExpressError('Page Not Found', 404));
+  next(new ExpressError('Page Not Found', 404));
 });
 
 app.use((err, req, res, next) => {
-	const { statusCode = 500 } = err;
-	if (!err.message) err.message = 'Something went wrong!';
-	res.status(statusCode).render('error', { err });
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Something went wrong!';
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
-	console.log('Serving on port 3000');
+  console.log('Serving on port 3000');
 });
