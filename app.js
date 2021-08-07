f46d3d580225b80352dad8a84628269d620464e8
cphsiung <chunpeihs@gmail.com>
2021-08-06 22:53:12 -0700

    Add security protection

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -14,6 +13,9 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const bobaRoutes = require('./routes/bobas');
 const reviewRoutes = require('./routes/reviews');
@@ -41,19 +43,75 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({replaceWith: '_'}));
 
 const sessionConfig = {
+  name:'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true.valueOf,
+    sameSite: "lax",
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //expire in a week in milliseconds
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+//helmet content security setup
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+  "https://fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://*.tiles.mapbox.com/",
+  "https://ka-f.fontawesome.com",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [
+  "https://use.fontawesome.com/",
+  "https://fonts.gstatic.com/"
+];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dx1fbfw74/",
+              "https://images.unsplash.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
+
 
 app.use(passport.initialize());
 app.use(passport.session());
